ccae7c917274414806f714a91803d42b9767e386
rladydwp88 <rladydwp88@gmail.com>
2025-04-09 17:49:53 +0900

    최초시작

--- Memo/src/main/java/edu/kh/memo/model/dto/MemoList.java
+++ Memo/src/main/java/edu/kh/memo/model/dto/MemoList.java
@@ -1,5 +1,18 @@
 package edu.kh.memo.model.dto;
 
-public class MemoList {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemoList {
+	private int memoNo;
+	private String memoTitle;
+	private String memoDetail;
+	private String memoDate;
+	private String memoUpdate;
 }
